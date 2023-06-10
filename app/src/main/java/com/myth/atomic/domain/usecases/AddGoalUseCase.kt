9ec4a6fe79b2957faa0fc620d6576ff49c80dd5b
MichaelGift <simiyugift@outlook.com>
2023-06-11 00:45:09 +0300

    Add AddGoalUseCase

--- app/src/main/java/com/myth/atomic/domain/usecases/AddGoalUseCase.kt
+++ app/src/main/java/com/myth/atomic/domain/usecases/AddGoalUseCase.kt
@@ -3,6 +3,6 @@ package com.myth.atomic.domain.usecases
 import com.myth.atomic.data.model.Goals
 import com.myth.atomic.domain.repository.Repository
 
-class AddGoalUseCase(val repository: Repository) {
+class AddGoalUseCase(private val repository: Repository) {
     suspend fun execute(goal: Goals) = repository.addGoal(goal)
 }
