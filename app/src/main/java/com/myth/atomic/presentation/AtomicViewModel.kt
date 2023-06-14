46dcfe1445d70afc92b77547c8fe957ab6367803
MichaelGift <simiyugift@outlook.com>
2023-06-14 18:40:59 +0300

    Add VieModelImplementations
    Add getAllGoals(), getAllJournals(), getAllReminders() and getAllTasks(). Then emitted the liveData

--- app/src/main/java/com/myth/atomic/presentation/AtomicViewModel.kt
+++ app/src/main/java/com/myth/atomic/presentation/AtomicViewModel.kt
@@ -1,4 +1,38 @@
 package com.myth.atomic.presentation
 
-class AtomicViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.myth.atomic.domain.usecases.AddGoalUseCase
+import com.myth.atomic.domain.usecases.AddJournalUseCase
+import com.myth.atomic.domain.usecases.GetAllGoalsUseCase
+import com.myth.atomic.domain.usecases.GetAllJournalsUseCase
+import com.myth.atomic.domain.usecases.GetAllRemindersUseCase
+import com.myth.atomic.domain.usecases.GetAllTasksUseCase
+
+class AtomicViewModel(
+    private val getAllGoalsUseCase: GetAllGoalsUseCase,
+    private val getAllJournalsUseCase: GetAllJournalsUseCase,
+    private val getAllRemindersUseCase: GetAllRemindersUseCase,
+    private val getAllTasksUseCase: GetAllTasksUseCase,
+    
+) : ViewModel() {
+    fun getAllGoals() = liveData {
+        val goalsList = getAllGoalsUseCase.execute()
+        emit(goalsList)
+    }
+
+    fun getAllJournals() = liveData {
+        val journalsList = getAllJournalsUseCase.execute()
+        emit(journalsList)
+    }
+
+    fun getAllReminders() = liveData {
+        val remindersList = getAllRemindersUseCase.execute()
+        emit(remindersList)
+    }
+
+    fun getAllTasks() = liveData {
+        val tasksList = getAllTasksUseCase.execute()
+        emit(tasksList)
+    }
 }
